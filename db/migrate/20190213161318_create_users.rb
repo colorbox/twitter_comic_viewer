dd340945b85c4f42c8f52ff67af3490a528dc66e
colorbox <colorbox222@gmail.com>
2019-02-14 01:51:51 +0900

    Add user model

---
+++ db/migrate/20190213161318_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :twitter_identifier, null: false
+      t.string :screen_name, null: false
+      t.string :name, null:false
+      t.string :access_token, null: false
+      t.string :access_token_secret, null: false
+
+      t.timestamps
+    end
+  end
+end
