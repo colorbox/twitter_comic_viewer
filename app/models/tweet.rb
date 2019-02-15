d4e6f468811a8e0f0b5da086ecc81d705408e118
colorbox <colorbox222@gmail.com>
2019-02-15 14:53:24 +0900

    Add media model

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,4 @@
 class Tweet < ApplicationRecord
   belongs_to :comic
+  has_many :media, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_14_160614) do
+ActiveRecord::Schema.define(version: 2019_02_15_055116) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,12 @@ ActiveRecord::Schema.define(version: 2019_02_14_160614) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_comics_on_user_id"
+  end
+
+  create_table "media", force: :cascade do |t|
+    t.string "image_url", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "tweets", force: :cascade do |t|
