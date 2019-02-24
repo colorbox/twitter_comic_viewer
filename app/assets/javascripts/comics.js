6a94a7f57727e00cf9148661a95ec925b093f776
colorbox <colorbox222@gmail.com>
2019-02-24 23:08:08 +0900

    Add comic viewer
      c.f) https://www.w3schools.com/howto/howto_js_slideshow.asp

---
+++ app/assets/javascripts/comics.js
@@ -0,0 +1,33 @@
+ // Place all the behaviors and hooks related to the matching controller here.
+ // All this logic will automatically be available in application.js.
+ // You can use CoffeeScript in this file: http://coffeescript.org/
+
+var slideIndex = 0;
+
+function slideNext() {
+  slideIndex++;
+  showSlide();
+}
+
+function slidePrev() {
+  slideIndex--;
+  showSlide();
+}
+
+function showSlide() {
+  var i;
+  comic = document.getElementsByClassName("medium");
+  for (i = 0; i < comic.length; i++) {
+    comic[i].style.display = "none";
+  }
+
+  if (slideIndex >= comic.length) {slideIndex = comic.length - 1}
+  if (0 > slideIndex) {slideIndex = comic.length - 1}
+  comic[slideIndex].style.display = "block";
+}
+
+$(document).ready(
+  function() {
+    showSlide();
+  }
+);
